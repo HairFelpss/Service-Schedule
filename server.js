16527cd533b3b8ce699f82ca769fb748b661862d
Felipe <felipe.jesus@sysmap.com.br>
2019-09-11 23:29:50 -0300

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 8001
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('home')
 })
 
 app.listen(port, () => console.log('listen to port =>', port))
